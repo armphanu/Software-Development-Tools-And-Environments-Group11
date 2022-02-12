f2bb13ccd4e734bbc07a5389d3bfabc8668e5a38
unknown <thanawat.2543@yahoo.co.th>
2022-02-12 18:49:22 +0700

    edit : แก้ไข หน้า home.vue ให้แสดง card ให้ดูง่ายขึ้นตาม task https://trello.com/c/SaA72hmk

--- front-end/src/views/home.vue
+++ front-end/src/views/home.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="container">
     <div class="col">
-      <div
-        class="card text-center"
-        v-for="(value, index) in postanduser"
-        :key="index"
-      >
+      <div v-for="(value, index) in post"
+        :key="index" >
+ 
+      <div class="card text-center">
         <div class="card-header">
-          <h5 v-show="value.title != 'undefind'">{{ value.title }}</h5>
-          <h6>{{ value.user_name }}</h6>
+          <h2>{{ value.title }}</h2>
         </div>
         <div class="card-body" v-show="value.description != 'undefind'">
           {{ value.description }}
         </div>
-        <div class="card-body">{{ value.email }} {{ value.phone_number }}</div>
+        <div class="card-footer">
+          <h5 style="color:red ">Tag</h5>
+          <span v-show="value.tag.length" v-for="(value1, index) in value.tag"
+        :key="index + 'post tag'" style="color:blue ">{{value1}} <label v-if="index != value.tag.length -1">,</label></span>
+          <label style="margin-left: 720px">post</label>
+        </div>
+        
+      </div>
+      <br>
+      </div>
+      <br>
+        <div  v-for="(value, index) in user" :key="index + 'user'" >
+        <div class="card text-center" >
+        <div class="card-header">
+          <h2>{{ value.user_name }}</h2>
+        </div>
+        <div class="card-body" v-show="value.description != 'undefind'">
+          {{ value.email }} {{ value.phone_number }}
+        </div>
+        <div class="card-footer" >
+           <h5 style="color:red ">Tag</h5>
+          <span v-show="value.tag.length" v-for="(value1, index) in value.tag"
+        :key="index + 'user tag'" style="color:blue ">{{value1}} <label v-if="index != value.tag.length -1">,</label></span>
+           <label style="margin-left: 720px">user</label>
+        </div>
+      </div>
+      <br>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 import axios from "axios";
 export default {
   data() {
     return {
-      postanduser: [],
+      post: [],
+      user:[]
     };
   },
   methods: {},
@@ -34,14 +58,14 @@ export default {
       .get("http://localhost:3000/post/" + localStorage.getItem("id"))
       .then((res) => {
         for (let i = 0; i < res.data.length; i++) {
-          this.postanduser.push(res.data[i]);
+          this.post.push(res.data[i]);
         }
       });
     axios
       .get("http://localhost:3000/user/" + localStorage.getItem("id"))
       .then((res) => {
         for (let i = 0; i < res.data.length; i++) {
-          this.postanduser.push(res.data[i]);
+          this.user.push(res.data[i]);
         }
       });
   },
