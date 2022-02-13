ac2a324ce11c5d489222531926eeabf75ccf3086
armphanu <armzazgakkub@gmail.com>
2022-02-13 11:56:22 +0700

    edit  : change username/password mongodb to secret

--- back-end/mongoose.js
+++ back-end/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://armphanu:phanu_123@cluster0.2rjb0.mongodb.net/Exercise', {
+mongoose.connect('mongodb+srv://<<username>>:<<password>>@cluster0.2rjb0.mongodb.net/Exercise', {
   useNewUrlParser: true,
 }).then(()=>{
     console.log('connect sucess')
