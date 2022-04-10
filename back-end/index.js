d04e47f318ae920804b06c11f5a689cd05ec64f3
unknown <thanawat.2543@yahoo.co.th>
2022-04-10 23:16:58 +0700

    edit : index js

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 const path = require("path")
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3332
 require('./mongoose')
 app.use(cors({
   origin:true,
