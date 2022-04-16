be07e50d4182553e7f96da8d9a3a265c58bc65c1
pomeiei <pomboivo@hotmail.com>
2022-04-16 18:52:10 +0700

    edit : model user

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -8,7 +8,13 @@ const userSchema = new Schema({
     email: String,
     first_name: String,
     last_name: String,
-    tag: Array
+    tag: Array,
+    sick:String,
+    weight:Number,
+    height:Number,
+    sex:String,
+    follow:Array
+
   })
   
 
