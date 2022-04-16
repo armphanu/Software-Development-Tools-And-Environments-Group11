d852e2554439d9a2fbb236c0f22120f99105a32e
BaySP <baybay.bayyer@gmail.com>
2022-04-16 16:37:28 +0700

    edit : Like, Comment, Report Feature

--- front-end/src/views/Like_Comment_Follow_Report.vue
+++ front-end/src/views/Like_Comment_Follow_Report.vue
@@ -139,3 +139,26 @@
       <br />
 
 </div>
+<!-- modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <input type="text" class="form-control" name="" id="" v-model="reportmodel" >
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-warning" @click="confrimreport()" data-dismiss="modal">Confirm report</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </div>
+</template>
